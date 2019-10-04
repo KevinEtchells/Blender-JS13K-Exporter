31c616501e4f6c5dbf7cf804447c36189ac47a30
Kevin Etchells <kevetchells@hotmail.com>
2019-10-04 07:32:00 +0100

    Create "Tiny3D" format spec + Blender exporter and A-Frame importer

---
+++ blender-to-tiny3d.py
@@ -0,0 +1,97 @@
+import bpy
+import math
+import zlib
+
+# 1 = type: (0 = box, 1 = cylinder, 2 = cone, 3 = plane, 4 = sphere)
+#     Cylinder: type segmentsRadial
+#     Cone: type segmentsRadial radiusBottom radiusTop
+# 2 = group colour i (based on materials. Convert group names to numbers) (color, without #) (i = interact class - only need to add # the first time a group appears)
+# 3 = position
+# 4 = scale (if 1 value, apply that to all 3 axis)
+# 5 = rotation (optional)
+
+def radToDeg (val):
+    return (val * 180) / math.pi
+
+def parseNumber (val): # converts number to string, and removes trailing zeros
+    text = str(round(val, 2))
+    if text[len(text) - 1] == '0':
+        text = text[0: len(text) - 1]
+    if text[len(text) - 1] == '.':
+        text = text[0: len(text) - 1]
+    return text
+
+def rgbToHex (r, g, b): # converts rgb values in the range of 0-1 to hex code
+    # TO DO: convert to 3 digits only
+    return '%02x%02x%02x' % (math.floor(r * 255), math.floor(g * 255), math.floor(b * 255))
+
+output = ''
+
+groupNames = ['general']
+groupsRefs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] # so we don't need to store the actual name, saving space
+
+for obj in bpy.data.objects:
+    
+    if (output != ''): # i.e. this is a new entity
+        output += '/'
+    
+    # work out which group this belongs to based on its material
+    group = 'general'
+    groupIndex = 0
+    groupText = ''
+    if obj.material_slots:
+        group = obj.material_slots[0].name
+    
+    # does this group already exist?
+    if group not in groupNames:
+        # create a new group
+        groupIndex = len(groupNames)
+        groupNames.append(group)
+        groupText = groupsRefs[groupIndex] + ' ' + rgbToHex(obj.material_slots[0].material.diffuse_color[0], obj.material_slots[0].material.diffuse_color[1], obj.material_slots[0].material.diffuse_color[2])
+        
+        # custom properties on the material:
+        # (only I for interact at the moment, but this may expand in future)
+        for customProp in obj.material_slots[0].material.keys():
+            if customProp == 'Interact':
+                groupText += ' I'
+
+    else:
+        groupIndex = groupNames.index(group)
+        groupText = groupsRefs[groupIndex]
+
+    # geometry
+    if 'Cube' in obj.name:
+        output += '0'
+    elif 'Plane' in obj.name:
+        output += '3'
+    elif 'Cone' in obj.name:
+        output += '2 '
+        output += str(len(obj.data.polygons) - 2) + ' '
+        if 'radiusBottom' not in obj.keys():
+            obj['radiusBottom'] = 1
+        if 'radiusTop' not in obj.keys():
+            obj['radiusTop'] = 0
+        output += str(obj['radiusBottom']) + ' '
+        output += str(obj['radiusTop'])
+    elif 'Cylinder' in obj.name:
+        output += '1 '
+        output += str(len(obj.data.polygons) - 2)
+    
+    output += ',' + groupText
+    
+    # position
+    output += ',' + parseNumber(obj.location.x) + ' ' + parseNumber(obj.location.z) + ' ' + parseNumber(obj.location.y)
+    
+    # scale
+    if obj.scale.x == obj.scale.y and obj.scale.x == obj.scale.z: # just write a single scale value
+        output += ',' + parseNumber(obj.scale.x * 2) + ','
+    else: 
+        output += ',' + parseNumber(obj.scale.x * 2) + ' ' + parseNumber(obj.scale.z * 2) + ' ' + parseNumber(obj.scale.y * 2)
+
+    # rotation
+    if obj.rotation_euler.x != 0 or obj.rotation_euler.y != 0 or obj.rotation_euler.z != 0:
+        output += ',' + parseNumber(radToDeg(obj.rotation_euler.x)) + ' ' + parseNumber(radToDeg(obj.rotation_euler.z)) + ' ' + parseNumber(radToDeg(obj.rotation_euler.y))
+
+# print output
+print(output)
+print('(' + str(round(len(output) / 1024, 2)) + 'KB uncompressed, ' + str(round(len(zlib.compress(output.encode('utf-8'))) / 1024, 2)) + 'KB compressed)')
---
+++ tiny3d-to-aframe.js
@@ -0,0 +1,106 @@
+/*
+1 = type: (0 = box, 1 = cylinder, 2 = cone, 3 = plane, 4 = sphere)
+    Cylinder: type segmentsRadial
+    Cone: type segmentsRadial radiusBottom radiusTop
+2 = group colour i (based on materials. Convert group names to numbers) (color, without #) (i = interact class - only need to add the first time a group appears)
+3 = position
+4 = scale (if 1 value, apply that to all 3 axis)
+5 = rotation (optional)
+*/
+
+const expandTiny3D = (function () {
+
+    const GEOMETRIES = ["box", "cylinder", "cone", "plane", "sphere"];
+    
+    return function (data) {
+
+        let scene = document.querySelector("a-scene");
+        let groups = []; // {id: 0, el: <a-entity>}
+
+        // loop through each entity
+        data.split("/").forEach(function (entity) {
+
+            // split entity into each component
+            let sections = entity.split(",");
+
+            let el = document.createElement("a-entity");
+
+
+            // 1. geometry
+            let geometryText = "primitive: ";
+            geometryData = sections[0].split(" ");
+            geometryText += GEOMETRIES[parseInt(geometryData[0])];
+            el.setAttribute("geometry", geometryText);
+            
+            // segmentsRadial
+            if (geometryData[0] === "1" || geometryData[0] === "2") {
+                geometryText += "; segmentsHeight: 1; segmentsRadial: " + geometryData[1];
+            }
+            
+            // cone - radiusBottom + radiusTop
+            if (geometryData[0] === "2") {
+                geometryText += "; radiusBottom: " + geometryData[2] + "; radiusTop: " + geometryData[3];
+            }
+            
+            // geometry attribute is added later, so we know whether to add "buffer: false"
+            
+            
+            // 3. position
+            el.setAttribute("position", sections[2]);
+            
+
+            // 4. scale
+            let scaleData = sections[3];
+            if (scaleData.indexOf(" ") === -1) { // it is just one value, apply that to all 3 axis
+                el.setAttribute("scale", scaleData + " " + scaleData + " " + scaleData);
+            } else { // copy it exactly
+                el.setAttribute("scale", scaleData);
+            }
+            
+            
+            // 5. rotation
+            if (sections.length >= 5) {
+                el.setAttribute("rotation", sections[4]);
+            }
+            
+
+            // 2. group - do this last so can add directly to group entity
+            let groupData = sections[1].split(" ");
+
+            // check if group already exists
+            let groupIndex = -1;
+            groups.forEach(function (group, index) {
+                if (group.id === groupData[0]) {
+                    groupIndex = index;
+                } 
+            });
+            if (groupIndex === -1) { // add new group
+                groups.push({
+                    id: groupData[0],
+                    el: document.createElement("a-entity")
+                });
+                groupIndex = groups.length - 1;
+                groups[groupIndex].el.setAttribute("merge", "");
+                groups[groupIndex].el.setAttribute("material", "color: #" + groupData[1]);
+                if (groupData.length >= 3 && groupData[2].indexOf("i") !== -1) { // add interactive class
+                    groups[groupIndex].el.className = "i";
+                }
+            }
+
+            // add element to group
+            geometryText += "; buffer: false";
+            groups[groupIndex].el.appendChild(el);
+            
+            // set this last, as "buffer: false" is only added at the group stage
+            el.setAttribute("geometry", geometryText);
+            
+        });
+        
+        // add each group to scene
+        groups.forEach(function (group) {
+            scene.appendChild(group.el); 
+        });
+        
+    };
+    
+}());
